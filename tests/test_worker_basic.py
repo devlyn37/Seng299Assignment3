cd1f5a943d6d4b321c376699474035aed370db66
Devlyn Dorfer <dev37-2009@hotmail.com>
2017-07-17 23:56:54 -0700

    Update test_worker_basic.py
    deleted faulty unit test

--- tests/test_worker_basic.py
+++ tests/test_worker_basic.py
@@ -50,19 +50,6 @@ class TestWorkerBasic(unittest.TestCase):
 
         self.assertEqual(len_to_crawl_after, len_to_crawl_before)
 
-    """
-    def test_worker_add_links_in_crawled(self):
-        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
-        worker.crawled = []
-
-        len_to_crawl_before = len(worker.to_crawl)
-        worker.add_links(["https://www.reddit.com/user/Chrikelnel"])
-        len_to_crawl_after = len(worker.to_crawl)
-
-        self.assertEqual(len_to_crawl_after, len_to_crawl_before)
-    """
-
-
 
 
 
